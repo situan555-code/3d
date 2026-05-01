8794178e322d6b403626e6bece3619ef672c7261
nautis neff <nautiskyrobertneff@gmail.com>
2026-04-30 20:13:13 -0400

    fix: convert OverlayContexts.js to .ts to resolve Vercel type error

--- src/portfolio/contexts/OverlayContexts.ts
+++ src/portfolio/contexts/OverlayContexts.ts
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 
 // This context provides the root 1024x768 global overlay node created by WicgHitbox
-export const GlobalOverlayContext = createContext(null);
+export const GlobalOverlayContext = createContext<HTMLElement | null>(null);
 
 // This context provides the specific window's internal scrolled content node created by Window.jsx
-export const WindowOverlayContext = createContext(null);
+export const WindowOverlayContext = createContext<HTMLElement | null>(null);
